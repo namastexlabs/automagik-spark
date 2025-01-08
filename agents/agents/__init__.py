e8887aa5f52bff8325950e99bc33acd55936ff6e
Cezar Vasconcelos <cezarvasconcelos.dev@gmail.com>
2025-01-08 20:52:52 +0000

    chore: cleanup unused folders and files

--- agents/agents/__init__.py
+++
@@ -1,4 +0,0 @@
-from .models import Base, Agent
-from scheduler.models import Schedule
-
-__all__ = ['Base', 'Agent', 'Schedule']
--- sync_flows/__init__.py
+++
@@ -1,3 +0,0 @@
-from .models import FlowDB, Base
-
-__all__ = ['FlowDB', 'Base']
